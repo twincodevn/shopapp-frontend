78ff26fccc0d32f1a87397908a2a69d98e366c84
Bravee06 <bravee300@gmail.com>
2024-01-22 00:06:33 +0700

    initial front end shopapp

---
+++ src/app/order-confirm/order-confirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-confirm.component.html',
+  styleUrl: './order-confirm.component.scss'
+})
+export class OrderConfirmComponent {
+
+}
