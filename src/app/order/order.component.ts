f3f600ece641e7d6bd48eace7cd0608167e574f2
Bravee06 <bravee300@gmail.com>
2024-01-22 08:39:35 +0700

    design home page

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { HeaderComponent } from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component'
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,FooterComponent],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
+import { HomeComponent } from './app/home/home.component';
+import {OrderComponent} from './app/order/order.component'
+bootstrapApplication(OrderComponent, appConfig)
   .catch((err) => console.error(err));
